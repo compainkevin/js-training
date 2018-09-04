75a1fce51a4c55825691b006fa7b606c58df5582
Arnaud Renaud <arnaud.renaud@gmail.com>
2018-09-04 17:35:40 +0200

    Implement basics math get advanced functions

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -10,10 +10,20 @@
  *
  */
 
-
+const jadenCase = (phrase) => {
+  return phrase
+    .split(' ')
+    .map(word => {
+      return word[0].toUpperCase() + word.slice(1)
+    })
+    .join(' ')
+}
 
 //* Begin of tests
 const assert = require('assert')
 
-assert.fail('You must write your own tests')
+assert.strictEqual(jadenCase("lol"), "Lol")
+assert.strictEqual(jadenCase("haha hihi"), "Haha Hihi")
+assert.strictEqual(jadenCase("haha Hihi"), "Haha Hihi")
+assert.strictEqual(jadenCase("ABC ABC"), "ABC ABC")
 // End of tests */
